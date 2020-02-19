12209143eb76493bdce089b425a882fa4380c3ac
Marcin Parda <marcin98parda@wp.pl>
2020-02-19 16:04:52 +0100

    add sorting task

--- src/components/App.js
+++ src/components/App.js
@@ -6,40 +6,7 @@ import TaskList from "./TaskList";
 class App extends Component {
   counter = 4;
   state = {
-    tasks: [
-      {
-        id: 0,
-        text: "Zagrać w Wiedźmina 3",
-        date: "2018-02-15",
-        important: true,
-        active: true,
-        finishDate: null
-      },
-      {
-        id: 1,
-        text: "Zrobić pranie",
-        date: "2020-12-15",
-        important: false,
-        active: false,
-        finishDate: null
-      },
-      {
-        id: 2,
-        text: "Przytyć 5 kilo",
-        date: "2019-03-18",
-        important: false,
-        active: true,
-        finishDate: null
-      },
-      {
-        id: 3,
-        text: "Zagrać na ukulele",
-        date: "2020-02-14",
-        important: true,
-        active: true,
-        finishDate: null
-      }
-    ]
+    tasks: []
   };
 
   deleteTask = id => {
--- src/components/TaskList.js
+++ src/components/TaskList.js
@@ -5,6 +5,26 @@ const TaskList = props => {
   const active = props.tasks.filter(task => task.active === true);
   const done = props.tasks.filter(task => task.active === false);
 
+  // done.sort((a, b) => b.finishDate - a.finishDate);
+  if (done.length >= 2) {
+    done.sort((a, b) => {
+      if (a.finishDate < b.finishDate) return 1;
+      if (a.finishDate > b.finishDate) return -1;
+      return 0;
+    });
+  }
+  console.log(active);
+  if (active.length >= 2) {
+    active.sort((a, b) => {
+      a = a.text.toLowerCase();
+      b = b.text.toLowerCase();
+      if (a < b) return -1;
+      if (a > b) return 1;
+      return 0;
+    });
+  }
+  console.log(active);
+
   const activeTasks = active.map(task => (
     <Task
       key={task.id}
@@ -34,7 +54,7 @@ const TaskList = props => {
         <h2>
           Zadania ukończone <em>({done.length})</em>
         </h2>
-        {done.length > 2 && (
+        {done.length > 5 && (
           <span style={{ fontSize: 10 }}>
             Wyświetlonych jest jedynie 5 ostatnich zadań
           </span>
