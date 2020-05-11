70c71a67b45803ba4f174b55dc5d983dcc64d37b
MarcinParda <marcin98parda@gmail.com>
2020-05-11 18:36:00 +0200

    Updates

---
+++ precache-manifest.f7328282c3c4ca152473eb08ff36c030.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "65d7d95da5aacb473ffa",
+    "url": "/to-do-list/static/css/main.0b43f932.chunk.css"
+  },
+  {
+    "revision": "65d7d95da5aacb473ffa",
+    "url": "/to-do-list/static/js/main.df575eb0.chunk.js"
+  },
+  {
+    "revision": "15bb3101340a9b514097",
+    "url": "/to-do-list/static/js/runtime~main.9900d293.js"
+  },
+  {
+    "revision": "2a032cb8a3c2e81f734b",
+    "url": "/to-do-list/static/js/2.dbdfff11.chunk.js"
+  },
+  {
+    "revision": "84537d80aa7c73eee02519107e628b3f",
+    "url": "/to-do-list/index.html"
+  }
+];
